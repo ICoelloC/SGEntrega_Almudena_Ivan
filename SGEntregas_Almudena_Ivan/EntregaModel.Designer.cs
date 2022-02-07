3a1e58785028d93e5c8c1de8e8738a9cd5246ce3
Almudena Ruedas Arévalo <aruedasa01@educastillalamancha.es>
2022-02-07 15:58:56 +0100

    Modificacion BD

--- SGEntregas_Almudena_Ivan/EntregaModel.Designer.cs
+++ SGEntregas_Almudena_Ivan/EntregaModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// La generación de código T4 está habilitada para el modelo 'C:\Users\Almudena\source\repos\Tema6\SGEntrega_Almudena_Ivan\SGEntregas_Almudena_Ivan\EntregasModel.edmx'. 
+﻿// La generación de código T4 está habilitada para el modelo 'C:\Users\Almudena\source\repos\Tema6\SGEntrega_Almudena_Ivan\SGEntregas_Almudena_Ivan\EntregaModel.edmx'. 
 // Para habilitar la generación de código heredada, cambie el valor de la propiedad del diseñador 'Estrategia de generación de código'
 // por 'ObjectContext heredado'. Esta propiedad está disponible en la ventana Propiedades cuando se abre
 // el modelo en el diseñador.
