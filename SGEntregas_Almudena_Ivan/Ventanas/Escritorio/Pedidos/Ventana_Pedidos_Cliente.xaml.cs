ad2d6a19499dee9638c1a37786c6579738842c5a
Iván Coello Cortés <ivancoellocortes@gmail.com>
2022-02-07 17:07:10 +0100

    Añadidas las ventanas para la gestión de pedidos de los clientes

---
+++ SGEntregas_Almudena_Ivan/Ventanas/Escritorio/Pedidos/Ventana_Pedidos_Cliente.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SGEntregas_Almudena_Ivan.Ventanas.Escritorio.Pedidos
+{
+    /// <summary>
+    /// Lógica de interacción para Ventana_Pedidos_Cliente.xaml
+    /// </summary>
+    public partial class Ventana_Pedidos_Cliente : Window
+    {
+        public Ventana_Pedidos_Cliente()
+        {
+            InitializeComponent();
+        }
+    }
+}
